1198684c8796aa97d69e5c0107f5a4d67569bc3c
Piyush <piyushb544@gmail.com>
2022-04-10 12:04:18 +0530

    fixed update profile loopholes

--- ShopAccountHandler/Members/forms.py
+++ ShopAccountHandler/Members/forms.py
@@ -45,19 +45,13 @@ class EditProfileForm(UserChangeForm):
     last_name = forms.CharField(
         max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     username = forms.CharField(
-        max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+        max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'readonly': 'readonly'}))
     last_login = forms.CharField(
-        max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    is_superuser = forms.CharField(
-        max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
-    is_staff = forms.CharField(
-        max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
-    is_active = forms.CharField(
-        max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+        max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'readonly': 'readonly'}))
     date_joined = forms.CharField(
-        max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+        max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'readonly': 'readonly'}))
 
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'username', 'password',
-                  'last_login', 'is_superuser', 'is_staff', 'is_active', 'date_joined')
+                  'last_login', 'date_joined')

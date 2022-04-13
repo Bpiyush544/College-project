423fe2cb96dea9efb39d17a583c22e7ad6916c9a
Piyush <piyushb544@gmail.com>
2022-04-13 12:08:42 +0530

    removed Profile model

--- ShopAccountHandler/RcptGenerator/models.py
+++ ShopAccountHandler/RcptGenerator/models.py
@@ -22,16 +22,16 @@ class Product(models.Model):
         return reverse('home')
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    bio = models.TextField()
-    profile_pic = models.ImageField(
-        null=True, blank=True, upload_to="images/profile/")
-    website_url = models.CharField(max_length=255, null=True, blank=True)
-    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+#     bio = models.TextField()
+#     profile_pic = models.ImageField(
+#         null=True, blank=True, upload_to="images/profile/")
+#     website_url = models.CharField(max_length=255, null=True, blank=True)
+#     facebook_url = models.CharField(max_length=255, null=True, blank=True)
 
-    def __str__(self):
-        return str(self.user)
+#     def __str__(self):
+#         return str(self.user)
 
 # class Profile(models.Model):
 #     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
